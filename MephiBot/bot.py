18a01581f2ecb5212d40bd625872db5bbdcb318a
mmarss <marselfx55@mail.ru>
2024-08-10 23:06:44 +0500

    update data

--- MephiBot/bot.py
+++ MephiBot/bot.py
@@ -2,6 +2,9 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.types import Message
 
 from config import TOKEN
 
@@ -18,6 +21,13 @@ async def cmd_start(message: types.Message):
 @dp.message(Command("help"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello! I will help you")
+
+@dp.message(Command("hello"))
+async def cmd_hello(message: Message):
+    await message.answer(
+        f"Hello, <b>{message.from_user.full_name}</b>",
+        parse_mode=ParseMode.HTML
+    )
 
 async def main():
     await dp.start_polling(bot)
