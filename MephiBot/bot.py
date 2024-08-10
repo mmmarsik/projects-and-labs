cfb83083ae12ffa24d07703c8923f4256ee6d9d6
mmarss <marselfx55@mail.ru>
2024-08-10 22:17:19 +0500

    update data

--- MephiBot/bot.py
+++ MephiBot/bot.py
@@ -1,23 +1,31 @@
 from aiogram import *
+import asyncio
+from aiogram.filters.command import Command
+
 
 
 
 TOKEN = "7317277427:AAGKGmLfxcE8BUiq9ketqQtY5jFykaetGNM"
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(msg: types.Message):
+@dp.message(Command("start"))
+async def cmd_start(msg: types.Message):
     await msg.answer(f"Я бот. Приятно познакомиться, {msg.from_user.first_name}")
 
 
-@dp.message_handler(content_types=['text'])
-async def get_text_messages(msg: types.Message):
-    if msg.text.lower() == 'привет':
-        await msg.answer('Привет!')
-    else:
-        await msg.answer('Не понимаю, что это значит.')
+# @dp.message_handler(content_types=['text'])
+# async def get_text_messages(msg: types.Message):
+#     if msg.text.lower() == 'привет':
+#         await msg.answer('Привет!')
+#     else:
+#         await msg.answer('Не понимаю, что это значит.')
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
