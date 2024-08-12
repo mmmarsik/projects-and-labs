7733d4d7642a9add60f45a358cb48032d357b144
mmarss <marselfx55@mail.ru>
2024-08-12 19:10:17 +0500

    update data

--- MephiBot/bot.py
+++ MephiBot/bot.py
@@ -61,20 +61,23 @@ async def accept_new_task(message: types.Message):
 @dp.message(F.text.lower() == "перенаправить текущую команду")
 async def redirect_task(message: types.Message):
     free_stations = game_info.get_free_stations()
-    if free_stations:
-        builder = ReplyKeyboardBuilder()
 
-        for station in free_stations:
-            builder.add(types.KeyboardButton(text=station))
-
-        builder.adjust(2)
-
-        await message.answer(
-            "Выберите станцию для перенаправления команды:",
-            reply_markup=builder.as_markup(resize_keyboard=True),
-        )
-    else:
+    if not free_stations:
         await message.reply("Нет свободных станций для перенаправления команды.")
+        return
+
+    builder = ReplyKeyboardBuilder()
+
+    for station in free_stations:
+        builder.add(types.KeyboardButton(text=station))
+
+    builder.adjust(3)
+
+    await message.answer(
+        "Выберите станцию для перенаправления команды:",
+        reply_markup=builder.as_markup(
+            resize_keyboard=True, one_time_keyboard=True),
+    )
 
 
 @dp.message(F.text.in_(game_info.get_free_stations()))
@@ -95,6 +98,7 @@ async def handle_station_selection(message: types.Message):
 
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
